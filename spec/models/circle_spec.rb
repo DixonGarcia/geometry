2a8f2574c63aa37c16cf5f97c2d93cdeb99d7ec8
Dixon Garcia <dixongarcia24@gmail.com>
2019-11-29 14:45:04 -0400

    add validation when radius is nil

--- spec/models/circle_spec.rb
+++ spec/models/circle_spec.rb
@@ -3,11 +3,15 @@ require 'rails_helper'
 RSpec.describe Circle, type: :model do
   describe '#get_area' do
     describe 'return error when the radius' do
-      it 'it is not a number' do
+      it 'is a nil value' do
+        circle = Circle.new
+        expect { circle.get_area }.to raise_exception('Radius should be a Number')
+      end
+      it 'is not a number' do
         circle = Circle.new(radius: 'string')
         expect { circle.get_area }.to raise_exception('Radius should be a Number')
       end
-      it 'it is not positive' do
+      it 'is not positive' do
         circle = Circle.new(radius: -1)
         expect { circle.get_area }.to raise_exception('Radius should be a Positive Number')
       end
